d85afe8eab43bb842ea358b3e6fc888e65a1a9ad
Miguel Ángel Durán <miduga@gmail.com>
2024-07-20 18:09:07 +0200

    Use static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import vercel from "@astrojs/vercel/serverless";
@@ -6,9 +6,9 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
+  output: "hybrid",
   adapter: vercel(),
   experimental: {
-    serverIslands: true
-  }
+    serverIslands: true,
+  },
 });
