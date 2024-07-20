68f355df337faa622f0bc921b237c0e6c2f1788c
Miguel Ángel Durán <miduga@gmail.com>
2024-07-20 15:31:46 +0200

    Add suport to experimental server islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel(),
+  experimental: {
+    serverIslands: true
+  }
 });
